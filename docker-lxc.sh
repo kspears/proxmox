d2303e39c7adf25d2d2cc487aea0a504082174dc
Kevin Spears <kevin@thespears.com>
2023-12-21 07:22:47 -0800

    added non-interactive

--- docker-lxc.sh
+++ docker-lxc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Not using Sudo on LXC so updating offical docker install commands to match
+export DEBIAN_FRONTEND=noninteractive
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get -y remove $pkg; done
 apt-get update && apt-get -y upgrade
-apt-get install ca-certificates curl gnupg
+apt-get -y install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
