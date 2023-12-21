2a9c6443064a456fff9039145f6d9e3dcdf774f1
Kevin Spears <kevin@thespears.com>
2023-12-21 06:56:43 -0800

    Create docker-lxc.sh

---
+++ docker-lxc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Not using Sudo on LXC so updating offical docker install commands to match
+
+$ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get -y remove $pkg; done
+apt-get update && apt-get -y upgrade
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
